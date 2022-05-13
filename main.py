2e01b75d72319826b45b86a9d13e92348ac1dce8
alpaka1999 <100361028+alpaka1999@users.noreply.github.com>
2022-05-13 12:07:16 +0200

    Updated: Startseite eingefügt, Bootstrap und generell rumgespielt.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from plotly.offline import plot
 
 app = Flask("Sports tracker")
 
+#testing stuff
 def get_data():
     jahr = [2015, 2016, 2017, 2018, 2019, 2021]
     loc = [1500, 3500, 12000, 9000, 10000, 4000]
@@ -19,26 +20,29 @@ def viz():
     div = plot(fig, output_type="div")
     return div
 
+#Startseite / Home
 @app.route('/')
-def hello_world():
-    auswertung_link = url_for("auswertung")
-    return render_template("index.html", name="Selina", link=auswertung_link, dinger=["Ablauf", "Sportart", "To Do"])
+def home():
+    return render_template("index.html", user="Selina")
 
+#Überesichts Seite mit Übersicht der Erfassten Daten
 @app.route("/uebersicht")
-def auswertung():
-    return "success"
+def uebersicht():
+    return render_template("uebersicht.html")
 
 @app.route("/auswertung")
-def index():
+def auswertung():
     div = viz()
-    return render_template('uebersicht.html', name="Selina", viz_div=div)
+    return render_template("auswertung.html", viz_div=div)
 
-@app.route('//', methods=["GET", "POST"])
-def form():
+@app.route('/tracker', methods=["GET", "POST"])
+def tracker():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('tracker.html')
     if request.method == "POST":
         return "Vielen Dank für die Datenerfassung."
+
+    return render_template('formular.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
